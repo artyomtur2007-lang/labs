7d561744819a095befb309104c68daf6cf371186
artyomtur2007-lang <artyomtur2007@gmail.com>
2025-09-30 15:21:47 +0300

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,16 @@
+class bank:
+    def __init__(self,name):
+        self.name=name
+        self.accounts={}
+        self.client={}
+    # def add_clients
+    # def open_account
+    # def close_account
+class account:
+    def __init__(self,client,currency):
+
+        # self.account_id
+        # def deposit(self, amount):
+class client:
+    # def __init__(self, name,
+    
