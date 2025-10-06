c54d0a1a2b559d09c0fcdbae6bee1dbb4b6beba9
artyomtur2007-lang <artyomtur2007@gmail.com>
2025-10-06 22:16:44 +0300

    Add files via upload

--- lab3.py
+++ lab3.py
@@ -3,14 +3,20 @@ class bank:
         self.name=name
         self.accounts={}
         self.client={}
-    # def add_clients
-    # def open_account
-    # def close_account
+    def add_clients(self,client):
+         self.client=client
+    def open_account(self,client,currency):
+        #если нет аккаунта то класс исключения,если уже есть валюта тоже
+    def close_account(self,):
 class account:
     def __init__(self,client,currency):
-
+        self.currency=currency
+        self.client=client
         # self.account_id
         # def deposit(self, amount):
 class client:
-    # def __init__(self, name,
-    
+    def __init__(self, name,clients_id=None):
+        self.name=name
+        #self.clients_id=
+        self.accounts={}
+    def __str__(self):
