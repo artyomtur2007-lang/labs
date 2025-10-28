d5116b7edbd0f4624952ebcfdd37ccf59621c314
Artyom Tur <artyomtur2007@gmail.com>
2025-10-28 14:31:57 +0300

    Add a new commit

--- task_2.py
+++ task_2.py
@@ -1,4 +1,4 @@
 str = input("Введите строку: ")
 new_str = str.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '').replace('A', '').replace('E', '').replace('I', '').replace('O', '').replace('U', '')
-print('Строка без гласных',new_str)
+print(f" Строка без гласных,{new_str}")
 
